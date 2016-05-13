3da75a03dabf8bf396e159fcf2cbef4a3c48da2d
Fernand Aramendi <fernando@devartis.com>
2016-05-13 15:42:35 -0300

    added hover style fixed multi toolbard problem updated css styles to latest highlight version

--- main.js
+++ main.js
@@ -15,12 +15,15 @@ function convert() {
     hljs.highlightBlock(block[0]);
     applyStylesheet(block);
     newElement.appendChild(block[0]);
+    selRange.insertNode(document.createElement("br"));
     selRange.insertNode(newElement);
+    selRange.insertNode(document.createElement("br"));
+
     selObj.removeAllRanges();
   }
 }
 function applyStylesheet(elem) {
-	elem
+  elem
 		.css('display', 'block')
 		.css('overflow-x', 'auto')
 		.css('padding', '0.5em')
@@ -28,68 +31,37 @@ function applyStylesheet(elem) {
 		.css('background', '#f8f8f8')
 		.css('-webkit-text-size-adjust', 'none')
 		.css('font-family', 'monospace');
-
-
-elem.find('.hljs-comment, .diff .hljs-header, .hljs-javadoc')
-  .css('color', '#998')
-  .css('font-style', 'italic')
-
-elem.find('.hljs-keyword, .css .rule .hljs-keyword, .hljs-winutils, .nginx .hljs-title, .hljs-subst, .hljs-request, .hljs-status')
-  .css('color', '#333')
-  .css('font-weight', 'bold')
-
-elem.find('.hljs-number, .hljs-hexcolor, .ruby .hljs-constant')
-  .css('color', '#008080')
-
-elem.find('.hljs-string, .hljs-tag .hljs-value, .hljs-phpdoc, .hljs-dartdoc, .tex .hljs-formula')
-  .css('color', '#d14')
-
-elem.find('.hljs-title, .hljs-id, .scss .hljs-preprocessor')
-  .css('color', '#900')
-  .css('font-weight', 'bold')
-
-elem.find('.hljs-list .hljs-keyword, .hljs-subst')
-  .css('font-weight', 'normal')
-
-elem.find('.hljs-class .hljs-title, .hljs-type, .vhdl .hljs-literal, .tex .hljs-command')
-  .css('color', '#458')
-  .css('font-weight', 'bold')
-
-elem.find('.hljs-tag, .hljs-tag .hljs-title, .hljs-rules .hljs-property, .django .hljs-tag .hljs-keyword')
-  .css('color', '#000080')
-  .css('font-weight', 'normal')
-
-elem.find('.hljs-attribute, .hljs-variable, .lisp .hljs-body')
-  .css('color', '#008080')
-
-elem.find('.hljs-regexp')
-  .css('color', '#009926')
-
-elem.find('.hljs-symbol, .ruby .hljs-symbol .hljs-string, .lisp .hljs-keyword, .clojure .hljs-keyword, .scheme .hljs-keyword, .tex .hljs-special, .hljs-prompt')
-  .css('color', '#990073')
-
-elem.find('.hljs-built_in')
-  .css('color', '#0086b3')
-
-elem.find('.hljs-preprocessor, .hljs-pragma, .hljs-pi, .hljs-doctype, .hljs-shebang, .hljs-cdata')
-  .css('color', '#999')
-  .css('font-weight', 'bold')
-
-elem.find('.hljs-deletion')
-  .css('background', '#fdd')
-
-elem.find('.hljs-addition')
-  .css('background', '#dfd')
-
-elem.find('.diff .hljs-change')
-  .css('background', '#0086b3')
-
-elem.find('.hljs-chunk')
-  .css('color', '#aaa')
-	
+  elem.find('.hljs')
+  .css('display', 'block')
+  .css('overflow-x', 'auto')
+  .css('padding', '0.5em')
+  .css('background', '#F0F0F0');
+  elem.find('.hljs,.hljs-subst')
+  .css('color', '#444');
+  elem.find('.hljs-comment')
+  .css('color', '#888888');
+  elem.find('.hljs-keyword,.hljs-attribute,.hljs-selector-tag,.hljs-meta-keyword,.hljs-doctag,.hljs-name')
+  .css('font-weight', 'bold');
+  elem.find('.hljs-type,.hljs-string,.hljs-number,.hljs-selector-id,.hljs-selector-class,.hljs-quote,.hljs-template-tag,.hljs-deletion')
+  .css('color', '#880000');
+  elem.find('.hljs-title,.hljs-section')
+  .css('color', '#880000')
+  .css('font-weight', 'bold');
+  elem.find('.hljs-regexp,.hljs-symbol,.hljs-variable,.hljs-template-variable,.hljs-link,.hljs-selector-attr,.hljs-selector-pseudo')
+  .css('color', '#BC6060');
+  elem.find('.hljs-literal')
+  .css('color', '#78A960');
+  elem.find('.hljs-built_in,.hljs-bullet,.hljs-code,.hljs-addition')
+  .css('color', '#397300');
+  elem.find('.hljs-meta')
+  .css('color', '#1f7199');
+  elem.find('.hljs-meta-string')
+  .css('color', '#4d99bf');
+  elem.find('.hljs-emphasis')
+  .css('font-style', 'italic');
+  elem.find('.hljs-strong')
+  .css('font-weight', 'bold');
 }
-
-var addButtonInterval;
 
 function hasSomeParentTheClass(element, classname) {
     if (element.className && element.className.split(' ').indexOf(classname)>=0) return true;
@@ -97,13 +69,18 @@ function hasSomeParentTheClass(element, classname) {
 }
 
 function addButton() {
-	if (!$('#codebtn').length > 0) {
-		var onclick = 'window.postMessage({ type: \'CONVERT\'}, \'*\')';
-		$( "div[command='+removeFormat']" ).after('<div id="codebtn" onclick="' + onclick + '" class="J-Z-I J-J5-Ji">{...}</div>');
-	}
-}
+  $( "div[role='toolbar'].J-Z" ).each(function() {
+    if ($(this).find('.codebtn').length == 0) {
+      var onclick = 'window.postMessage({ type: \'CONVERT\'}, \'*\')';
+      var btn = $('<div onclick="' + onclick + '" class="codebtn J-Z-I J-J5-Ji" data-tooltip="Syntax Highlight" role="button">{...}</div>');
+      $(this).append(btn);
 
-addButtonInterval = setInterval(addButton, 3000);
+      $(this).find('.codebtn').hover(function() { $(this).toggleClass('J-Z-I-JW')});
+    }
+	});
+}
+// Check every 3 seconds if there's a new toolbar without our button
+var addButtonInterval = setInterval(addButton, 3000);
 
 window.addEventListener("message", function(event) {
   // We only accept messages from ourselves
